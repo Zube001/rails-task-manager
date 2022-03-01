b8970f686ecc636ae201040c2179d47a4a3fab56
Nzube Ifediba <ifedibazube@gmail.com>
2022-03-01 14:43:57 +0400

    index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # list tasks/ read all the tasks
+  get '/tasks', to: 'tasks#index'
+  # view details of a task / read one task
+  get '/tasks/:id', to: 'tasks#show'
+  # # add a new task / create a task
+  # get '/tasks/new', to: 'tasks#new'
+  # post '/tasks', to: 'tasks#create'
+  # # update tasks (edit a task)
+  # get '/tasks/:id/edit', to: 'tasks#edit'
+  # patch '/tasks/:id', to: 'tasks#update'
+  # # remove a tasks
+  # delete '/tasks/:id', to: 'tasks/destroy'
 end
